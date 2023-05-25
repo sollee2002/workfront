f2c335ab1f2ecff4f887e201583faba5546b97ec
sollee2002 <sollee2002@gamil.com>
2023-05-25 09:45:40 +0900

    0525

---
+++ 3JS/2. 변수와 자료형/06_이벤트.js
@@ -0,0 +1,8 @@
+
+window.onload = function() {
+    //고전 이벤트 모델
+    btn1.onclick = function() {
+    console.log('고전 이벤트 모델', this);
+    }
+
+}
---
+++ 3JS/3. 실습/js/1_toggle-classList_2.js
@@ -0,0 +1,5 @@
+    window.onload = function() {
+        view.addeventListener('click', function() {
+      detail.classList.toggle('hidden');
+    })
+}
